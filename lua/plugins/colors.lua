7b7bb0098b9014aeedb700201581728f81439ed6
TengNg <nguyentin148@gmail.com>
2023-12-23 07:41:06 +0700

    feat: remove unused + reconfig some plugins

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,90 @@
+function on_background_change(settings)
+    local ok, err = pcall(function() require(settings.theme) end)
+    if not ok then
+        print("Failed to load colorscheme, make sure it's installed")
+        return
+    end
+
+    local bg_mode = vim.o.background
+    local config = settings[bg_mode]
+    if config.setup ~= nil then
+        require(settings.theme).setup(config.setup)
+    end
+
+    vim.cmd("colorscheme " .. config.color)
+end
+
+return {
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        enabled = true,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local settings = {
+                theme = "rose-pine",
+                dark = {
+                    color = "rose-pine-moon",
+                    setup = {
+                        styles = {
+                            bold = true,
+                            italic = false,
+                            transparency = true,
+                        },
+                    }
+                },
+                light = {
+                    color = "rose-pine-dawn",
+                    setup = {
+                        styles = {
+                            bold = true,
+                            italic = false,
+                            transparency = false,
+                        },
+                    }
+                },
+            }
+            on_background_change(settings)
+            vim.api.nvim_create_autocmd("OptionSet", {
+                pattern = "background",
+                callback = function()
+                    on_background_change(settings)
+                end
+            })
+        end,
+    },
+
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     priority = 1000,
+    --     lazy = false,
+    --     config = function()
+    --         require("gruvbox").setup({
+    --             italic = {
+    --                 strings = false,
+    --                 emphasis = true,
+    --                 comments = false,
+    --                 operators = false,
+    --                 folds = true,
+    --             },
+    --             transparent_mode = true,
+    --             contrast = "hard",
+    --         })
+    --         vim.cmd[[colorscheme gruvbox]]
+    --     end,
+    -- },
+
+    -- {
+    --     "savq/melange-nvim",
+    --     enabled = true,
+    --     lazy = false,
+    --     priority = 1000,
+    --     init = function()
+    --         vim.g.melange_enable_font_variants = false
+    --         vim.cmd[[colorscheme melange]]
+    --         -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --         -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    --     end,
+    -- },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,40 +1,23 @@
 return {
     {
-        "nvim-lualine/lualine.nvim",
-        enabled = true,
-        config = function()
-            require('lualine').setup {
-                options = {
-                    theme = 'auto',
-                    globalstatus = false,
-                    icons_enabled = false,
-                    component_separators = { left = '', right = '' },
-                    section_separators = { left = '', right = '' },
-                },
-                sections = {
-                    lualine_a = {},
-                    lualine_b = { 'branch', 'diff' },
-                    lualine_c = { { 'filename', path =  1 } },
-                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                    lualine_y = { 'progress' },
-                    lualine_z = { 'location' }
-                },
-                inactive_sections = {
-                    lualine_c = { { 'filename', path =  1 } },
-                },
-            }
-        end
+        "nvim-tree/nvim-web-devicons",
+        lazy = true,
     },
 
     {
-        "folke/zen-mode.nvim",
-        cmd = "ZenMode",
-        opts = {
-            plugins = {
-                gitsigns = true,
-                tmux = true,
-            },
-        },
-        keys = { { "\\z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-    },
+        "echasnovski/mini.statusline",
+        config = function()
+            local statusline = require("mini.statusline")
+            statusline.setup({ use_icons = vim.g.have_nerd_font })
+            statusline.section_location = function()
+                return '%2l:%-2v'
+            end
+            statusline.section_lsp = function()
+                return ""
+            end
+            statusline.section_diagnostics = function()
+                return ""
+            end
+        end
+    }
 }
