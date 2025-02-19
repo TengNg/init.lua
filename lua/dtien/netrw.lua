abf3eb7f87152e74563c83b34d0ad32d9ac506b7
TengNg <nguyentin148@gmail.com>
2025-02-19 20:52:24 +0700

    feat: updates + oil.nvim + treesitter-context
    Add treesitter-context
    Config colorscheme
    Disable lsp-sematic-tokens

--- lua/dtien/netrw.lua
+++ lua/dtien/netrw.lua
@@ -1,37 +1,37 @@
--- vim.g.loaded_netrwPlugin = 1
--- vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 
-vim.g.netrw_banner = 0
-vim.g.netrw_altfile = 1
--- vim.g.netrw_winsize = 30
-vim.g.netrw_hide = 0
-
-vim.keymap.set("n", "-", ":Explore<cr>")
-vim.keymap.set("n", "<leader>vn", ":Explore<cr>")
-vim.keymap.set("n", "<leader>vl", ":Lexplore %:p:h<cr>")
-
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("FileType", {
-    pattern = "netrw",
-    desc = "Better mappings for netrw",
-
-    callback = function(desc)
-        local bind = function(lhs, rhs)
-            vim.keymap.set(
-                "n",
-                lhs,
-                rhs,
-                { remap = true, buffer = true, silent = true }
-            )
-        end
-
-        -- edit new file
-        bind("a", "%")
-
-        -- open quickmap-help
-        bind("?", ":help netrw-quickmap<CR>")
-
-        -- -- open file
-        -- bind("o", "<CR>")
-    end,
-})
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_altfile = 1
+-- -- vim.g.netrw_winsize = 30
+-- vim.g.netrw_hide = 0
+--
+-- vim.keymap.set("n", "-", ":Explore<cr>")
+-- vim.keymap.set("n", "<leader>vn", ":Explore<cr>")
+-- vim.keymap.set("n", "<leader>vl", ":Lexplore %:p:h<cr>")
+--
+-- local autocmd = vim.api.nvim_create_autocmd
+-- autocmd("FileType", {
+--     pattern = "netrw",
+--     desc = "Better mappings for netrw",
+--
+--     callback = function(desc)
+--         local bind = function(lhs, rhs)
+--             vim.keymap.set(
+--                 "n",
+--                 lhs,
+--                 rhs,
+--                 { remap = true, buffer = true, silent = true }
+--             )
+--         end
+--
+--         -- edit new file
+--         bind("a", "%")
+--
+--         -- open quickmap-help
+--         bind("?", ":help netrw-quickmap<CR>")
+--
+--         -- -- open file
+--         -- bind("o", "<CR>")
+--     end,
+-- })
