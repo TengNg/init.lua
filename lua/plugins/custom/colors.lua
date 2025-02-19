5e23a04bf51d5038bcc6e0dfc3ddd3653eb2bc24
TengNg <nguyentin148@gmail.com>
2025-02-19 20:52:24 +0700

    feat: updates
    Add treesitter-context
    Add linter: cspell
    Config colorscheme
    Disable lsp-sematic-tokens

--- lua/plugins/custom/colors.lua
+++ lua/plugins/custom/colors.lua
@@ -1,37 +1,28 @@
 return {
     {
-        "sainnhe/gruvbox-material",
+        "savq/melange-nvim",
+        priority = 1000,
+        enabled = true,
+        lazy = false,
         config = function()
-            vim.g.gruvbox_material_enable_italic = true
-            vim.g.gruvbox_material_transparent_background = true
-            vim.cmd([[colorscheme gruvbox-material]])
+            vim.g.melange_enable_font_variants = { italic = false }
+            vim.cmd("colorscheme melange")
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "StatusLine", { bg = "#34302C" })
         end,
     },
 
     -- {
-    --     "savq/melange-nvim",
-    --     priority = 1000,
-    --     enabled = false,
-    --     lazy = false,
-    --     config = function()
-    --         vim.g.melange_enable_font_variants = { italic = false }
-    --         vim.cmd("colorscheme melange")
-    --         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    --         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    --         vim.api.nvim_set_hl(0, "StatusLine", { bg = "#34302C" })
-    --     end,
-    -- },
-
-    -- {
     --     "ellisonleao/gruvbox.nvim",
     --     priority = 1000,
-    --     enabled = true,
+    --     enabled = false,
     --     lazy = false,
     --     config = function()
     --         require("gruvbox").setup({
     --             italic = {
     --                 strings = false,
-    --                 emphasis = true,
+    --                 emphasis = false,
     --                 comments = false,
     --                 operators = false,
     --                 folds = true,
@@ -53,10 +44,47 @@ return {
     --             styles = {
     --                 bold = true,
     --                 italic = false,
-    --                 transparency = true,
+    --                 transparency = false,
     --             },
+    --             palette = {
+    --                 main = {
+    --                     base = "#111111",
+    --                     -- overlay = '#363738',
+    --                 },
+    --                 moon = {
+    --                     base = "#111111",
+    --                     -- overlay = '#363738',
+    --                 },
+    --             },
+    --             highlight_groups = {
+    --                 Directory = { fg = "#68998a" },
+    --                 StatusLine = { fg = "subtle", bg = "overlay", blend = 20 },
+    --                 ColorColumn = { fg = "subtle", bg = "overlay", blend = 20 },
+    --             },
+    --             before_highlight = function(group, highlight, palette)
+    --                 if highlight.fg == palette.pine then
+    --                     highlight.fg = "#6e94b2"
+    --                 end
+    --                 if highlight.fg == palette.foam then
+    --                     highlight.fg = "#b4d4cf"
+    --                 end
+    --                 if highlight.fg == palette.gold then
+    --                     highlight.fg = "#e8b589"
+    --                 end
+    --             end,
     --         })
     --         vim.cmd("colorscheme rose-pine-moon")
+    --     end,
+    -- },
+
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     enabled = true,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.g.gruvbox_material_enable_italic = true
+    --         vim.g.gruvbox_material_transparent_background = true
+    --         vim.cmd([[colorscheme gruvbox-material]])
     --     end,
     -- },
 
