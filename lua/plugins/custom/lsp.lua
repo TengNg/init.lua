f372fb6bab6c037f22fd14c1c175eca78479d5c1
TengNg <nguyentin148@gmail.com>
2025-03-15 16:58:11 +0700

    feat(linter): setup cspell

--- lua/plugins/custom/lsp.lua
+++ lua/plugins/custom/lsp.lua
@@ -148,42 +148,4 @@ return {
             })
         end,
     },
-
-    {
-        "mfussenegger/nvim-lint",
-        ft = { "ruby", "php" },
-        config = function()
-            local lint = require("lint")
-
-            lint.linters_by_ft = {
-                ruby = { "ruby" },
-                php = { "php" },
-            }
-
-            local ns = require("lint").get_namespace("rubocop")
-            vim.diagnostic.config({
-                -- float = { border = "single" }, -- :help nvim_open_win()
-                underline = {
-                    severity = { min = vim.diagnostic.severity.ERROR },
-                },
-                virtual_text = {
-                    severity = { min = vim.diagnostic.severity.WARN },
-                },
-                signs = true,
-            }, ns)
-
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-                callback = function()
-                    lint.try_lint()
-                end,
-            })
-
-            if vim.bo.filetype == "ruby" then
-                vim.api.nvim_create_user_command("Rubocop", function()
-                    vim.diagnostic.reset(ns)
-                    lint.try_lint("rubocop")
-                end, {})
-            end
-        end,
-    },
 }
