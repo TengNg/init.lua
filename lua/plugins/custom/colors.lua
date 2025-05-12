d0fd21f6e3afbe460f1f6fb8824cdcd15f982b29
TengNg <nguyentin148@gmail.com>
2025-05-12 21:45:00 +0700

    feat: mason & mason-lspconfig v2.0

--- lua/plugins/custom/colors.lua
+++ lua/plugins/custom/colors.lua
@@ -4,7 +4,7 @@ return {
         enabled = true,
         priority = 1000,
         config = function()
-            vim.g.gruvbox_material_foreground = "mixed"
+            vim.g.gruvbox_material_foreground = "material"
             vim.g.gruvbox_material_enable_italic = true
             vim.g.gruvbox_material_transparent_background = true
             vim.cmd([[colorscheme gruvbox-material]])
