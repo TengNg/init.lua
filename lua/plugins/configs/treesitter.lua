318170315f0ffeafc4bcecea847a1d8243eeb4f4
TengNg <nguyentin148@gmail.com>
2025-08-02 10:18:49 +0700

    automated commit 2025-08-09 16:58:02

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -47,56 +47,56 @@ require("nvim-treesitter.configs").setup({
         },
     },
 
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["at"] = "@tag.outer",
-                ["it"] = "@tag.inner",
-            },
-        },
-        -- move = {
-        --     enable = true,
-        --     set_jumps = true, -- whether to set jumps in the jumplist
-        --     goto_next_start = {
-        --         [']m'] = '@function.outer',
-        --         [']]'] = '@class.outer',
-        --     },
-        --     goto_next_end = {
-        --         [']M'] = '@function.outer',
-        --         [']['] = '@class.outer',
-        --     },
-        --     goto_previous_start = {
-        --         ['[m'] = '@function.outer',
-        --         ['[['] = '@class.outer',
-        --     },
-        --     goto_previous_end = {
-        --         ['[M'] = '@function.outer',
-        --         ['[]'] = '@class.outer',
-        --     },
-        -- },
-        -- swap = {
-        --     enable = true,
-        --     swap_next = {
-        --         ['<leader>a'] = '@parameter.inner',
-        --         -- ['<leader>c'] = '@class.outer',
-        --         -- ['<leader>m'] = '@function.outer',
-        --     },
-        --     swap_previous = {
-        --         ['<leader>A'] = '@parameter.inner',
-        --         -- ['<leader>C'] = '@class.outer',
-        --         -- ['<leader>M'] = '@function.outer',
-        --     },
-        -- },
-    },
+    -- textobjects = {
+    --     select = {
+    --         enable = true,
+    --         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+    --         keymaps = {
+    --             -- You can use the capture groups defined in textobjects.scm
+    --             ["aa"] = "@parameter.outer",
+    --             ["ia"] = "@parameter.inner",
+    --             ["af"] = "@function.outer",
+    --             ["if"] = "@function.inner",
+    --             ["ac"] = "@class.outer",
+    --             ["ic"] = "@class.inner",
+    --             ["at"] = "@tag.outer",
+    --             ["it"] = "@tag.inner",
+    --         },
+    --     },
+    --     -- move = {
+    --     --     enable = true,
+    --     --     set_jumps = true, -- whether to set jumps in the jumplist
+    --     --     goto_next_start = {
+    --     --         [']m'] = '@function.outer',
+    --     --         [']]'] = '@class.outer',
+    --     --     },
+    --     --     goto_next_end = {
+    --     --         [']M'] = '@function.outer',
+    --     --         [']['] = '@class.outer',
+    --     --     },
+    --     --     goto_previous_start = {
+    --     --         ['[m'] = '@function.outer',
+    --     --         ['[['] = '@class.outer',
+    --     --     },
+    --     --     goto_previous_end = {
+    --     --         ['[M'] = '@function.outer',
+    --     --         ['[]'] = '@class.outer',
+    --     --     },
+    --     -- },
+    --     -- swap = {
+    --     --     enable = true,
+    --     --     swap_next = {
+    --     --         ['<leader>a'] = '@parameter.inner',
+    --     --         -- ['<leader>c'] = '@class.outer',
+    --     --         -- ['<leader>m'] = '@function.outer',
+    --     --     },
+    --     --     swap_previous = {
+    --     --         ['<leader>A'] = '@parameter.inner',
+    --     --         -- ['<leader>C'] = '@class.outer',
+    --     --         -- ['<leader>M'] = '@function.outer',
+    --     --     },
+    --     -- },
+    -- },
 })
 
 -- require("treesitter-context").setup({
--- lua/plugins/custom/treesitter.lua
+++ lua/plugins/custom/treesitter.lua
@@ -1,10 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        -- "nvim-treesitter/nvim-treesitter-context",
-    },
+    -- dependencies = {
+    --     "nvim-treesitter/nvim-treesitter-textobjects",
+    --     "nvim-treesitter/nvim-treesitter-context",
+    -- },
     build = ":TSUpdate",
     config = function()
         require("plugins/configs/treesitter")
